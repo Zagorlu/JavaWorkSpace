be206a2c67b4ac9a8f6c393864398be0636f920f
Emre Demirkol <tahaemredemirkol@gmail.com>
2018-08-06 13:08:01 +0300

    - Some edit

--- App/App.java
+++ App/App.java
@@ -1,23 +1,7 @@
 package App;
 
-import java.io.File;
-
 public class App {
 	public static void main(String[] args) {
-		try {
-
-			//RandomAccessFile text = new RandomAccessFile("C:\\Users\\z003x7jw\\Desktop\\BASE_DEVICE_INFO.txt","rw");
-			
-			File file  = new File("C:\\Users\\z003x7jw\\Desktop\\BASE_DEVICE_INFO.txt");
-			
-			
-			
-			
-			
-	
-		} catch (Throwable e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
+		
 	}
 }
